a001ed4247f1b979b7c677117585cea5fb4ac251
Hri7566 <hri7566@gmail.com>
2024-05-06 13:23:05 -0400

    Version bump

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mpp-hats
 // @namespace    http://github.com/Hri7566/hats
-// @version      2.0.5
+// @version      2.0.6
 // @description  Hats for MPP.net
 // @author       Hri7566
 // @match        https://www.multiplayerpiano.net/*
