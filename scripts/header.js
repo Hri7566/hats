87cd37e003953d1add3fbe4ad81573df16cb870c
Hri7566 <hri7566@gmail.com>
2025-07-15 01:25:26 -0400

    Update header

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mpp-hats
 // @namespace    http://github.com/Hri7566/hats
-// @version      2.1.1
+// @version      2.1.2
 // @description  Hats for MPP.net
 // @author       Hri7566
 // @match        https://www.multiplayerpiano.net/*
